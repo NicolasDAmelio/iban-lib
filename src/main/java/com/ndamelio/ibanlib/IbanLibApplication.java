381728fd726bf72eb9459ac90eea6c0eb294997e
Nicolas D'Amelio <ndamelio@capitaria.com>
2019-03-04 22:37:39 -0300

    First commit

---
+++ src/main/java/com/ndamelio/ibanlib/IbanLibApplication.java
@@ -0,0 +1,13 @@
+package com.ndamelio.ibanlib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbanLibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IbanLibApplication.class, args);
+	}
+
+}
